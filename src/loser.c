c55f95b2d20e909b09f9a67d0d3f8863ad7a62d3
writeitinc <writeitinc99@gmail.com>
2023-05-01 00:57:04 -0600

    add LSString & LSStringSpan constructors & destructors
    - Base Constructors/Destructors:
      - ls_string_create()
      - ls_string_destroy()
      - ls_sspan_create()
    
    - Convenience Constructors:
      - ls_string_clone()
      - ls_string_from_sspan()
      - ls_string_from_chars()
      - ls_string_from_cstr()
      - ls_sspan_from_string()
      - ls_sspan_from_chars()
      - ls_sspan_from_cstr()
    
    - Substring/Subspan Constructors:
      - ls_string_substr()
      - ls_string_subspan()
      - ls_sspan_substr()
      - ls_sspan_subspan()

--- src/loser.c
+++ src/loser.c
@@ -2,5 +2,35 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <string.h>
 
 #include <tyrant/tyrant.h>
+
+LSString ls_string_create(const LSByte *bytes, size_t len)
+{
+	if (!bytes) {
+		goto err_exit;
+	}
+
+	LSByte *bytes_cpy = TYRANT_ALLOC_ARR(bytes, len + 1);
+	if (!bytes_cpy) {
+		goto err_exit;
+	}
+
+	memcpy(bytes_cpy, bytes, len);
+	bytes_cpy[len] = '\0';
+
+	return (LSString){
+		.bytes = bytes_cpy,
+		.len = len
+	};
+
+err_exit:
+	return (LSString){ 0 };
+}
+
+void ls_string_destroy(LSString *string)
+{
+	LSByte *bytes_mutable = (LSByte *)string->bytes;
+	tyrant_free(bytes_mutable);
+}
--- src/loser.h
+++ src/loser.h
@@ -3,6 +3,7 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <string.h>
 
 typedef unsigned char LSByte;
 
@@ -22,9 +23,43 @@ typedef struct LSByteBuffer {
 	LSByte *bytes;
 } LSByteBuffer;
 
+// Validity Checks
+
 static inline bool LS_STRING_VALID(LSString string);
 static inline bool LS_SSPAN_VALID(LSStringSpan sspan);
 static inline bool LS_BBUF_VALID(LSByteBuffer bbuf);
+
+// Base Constructors/Destructors
+
+LSString ls_string_create(const LSByte *bytes, size_t len);
+void ls_string_destroy(LSString *string);
+
+static inline LSStringSpan ls_sspan_create(const LSByte *bytes, size_t len);
+
+// Convenience Constructors
+
+static inline LSString ls_string_clone(LSString string);
+static inline LSString ls_string_from_sspan(LSStringSpan sspan);
+static inline LSString ls_string_from_chars(const char *chars, size_t len);
+static inline LSString ls_string_from_cstr(const char *cstr);
+
+static inline LSStringSpan ls_sspan_from_string(LSString string);
+static inline LSStringSpan ls_sspan_from_chars(const char *chars, size_t len);
+static inline LSStringSpan ls_sspan_from_cstr(const char *cstr);
+
+// Substring/Subspan Constructors
+
+static inline LSString ls_string_substr(LSString string, size_t start,
+		size_t len);
+static inline LSStringSpan ls_string_subspan(LSString string, size_t start,
+		size_t len);
+
+static inline LSString ls_sspan_substr(LSStringSpan sspan, size_t start,
+		size_t len);
+static inline LSStringSpan ls_sspan_subspan(LSStringSpan sspan, size_t start,
+		size_t len);
+
+// Inline Definitions
 
 static inline bool LS_STRING_VALID(LSString string)
 {
@@ -39,6 +74,94 @@ static inline bool LS_SSPAN_VALID(LSStringSpan sspan)
 static inline bool LS_BBUF_VALID(LSByteBuffer bbuf)
 {
 	return bbuf.bytes != NULL;
+}
+
+static inline LSStringSpan ls_sspan_create(const LSByte *bytes, size_t len)
+{
+	return (LSStringSpan){
+		.start = bytes,
+		.len = len
+	};
+}
+
+static inline LSString ls_string_clone(LSString string)
+{
+	return ls_string_create(string.bytes, string.len);
+}
+
+static inline LSString ls_string_from_sspan(LSStringSpan sspan)
+{
+	return ls_string_create(sspan.start, sspan.len);
+}
+
+static inline LSString ls_string_from_chars(const char *chars, size_t len)
+{
+	return ls_string_create((const LSByte *)chars, len);
+}
+
+static inline LSString ls_string_from_cstr(const char *cstr)
+{
+	if (cstr == NULL) {
+		return (LSString){ 0 };
+	}
+
+	return ls_string_from_chars(cstr, strlen(cstr));
+}
+
+static inline LSStringSpan ls_sspan_from_string(LSString string)
+{
+	return (LSStringSpan){
+		.start = string.bytes,
+		.len = string.len
+	};
+}
+
+static inline LSStringSpan ls_sspan_from_chars(const char *chars, size_t len)
+{
+	return (LSStringSpan){
+		.start = (const LSByte *)chars,
+		.len = len
+	};
+}
+
+static inline LSStringSpan ls_sspan_from_cstr(const char *cstr)
+{
+	if (cstr == NULL) {
+		return (LSStringSpan){ 0 };
+	}
+
+	return ls_sspan_from_chars(cstr, strlen(cstr));
+}
+
+static inline LSString ls_string_substr(LSString string, size_t start,
+		size_t len)
+{
+	return ls_string_from_sspan(ls_string_subspan(string, start, len));
+}
+
+static inline LSStringSpan ls_string_subspan(LSString string, size_t start,
+		size_t len)
+{
+	return ls_sspan_subspan(ls_sspan_from_string(string), start, len);
+}
+
+static inline LSString ls_sspan_substr(LSStringSpan sspan, size_t start,
+		size_t len)
+{
+	return ls_string_from_sspan(ls_sspan_subspan(sspan, start, len));
+}
+
+static inline LSStringSpan ls_sspan_subspan(LSStringSpan sspan, size_t start,
+		size_t len)
+{
+	if (!LS_SSPAN_VALID(sspan)
+			|| start > sspan.len
+			|| len > sspan.len
+			|| start > sspan.len - len) {
+		return (LSStringSpan){ 0 };
+	}
+
+	return ls_sspan_create(&sspan.start[start], len);
 }
 
 #endif // loser_h

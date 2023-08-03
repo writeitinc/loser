0a008ee8fc61f8081caac3590d4a1d54293ccf91
writeitinc <writeitinc99@gmail.com>
2023-08-02 22:24:30 -0600

    add test_conversions()

--- tests/test.c
+++ tests/test.c
@@ -4,6 +4,7 @@
 #include <loser/loser.h>
 
 static void test_constructors(void);
+static void test_conversions(void);
 
 static const LSByte NONEMPTY_BYTES[] = "deadbeef";
 static size_t NONEMPTY_LEN = sizeof(NONEMPTY_BYTES) - 1;
@@ -11,6 +12,7 @@ static size_t NONEMPTY_LEN = sizeof(NONEMPTY_BYTES) - 1;
 int main(void)
 {
 	test_constructors();
+	test_conversions();
 
 	return 0;
 }
@@ -59,4 +61,244 @@ void test_constructors(void)
 		assert(ls_sspan_is_valid(from_nonempty));
 		assert(!ls_sspan_is_valid(from_null));
 	}
+}
+
+void test_conversions(void)
+{
+	LSString nonempty_string = ls_string_create(NONEMPTY_BYTES, NONEMPTY_LEN);
+	LSShortString nonempty_short_string = ls_short_string_create(NONEMPTY_BYTES, NONEMPTY_LEN);
+	LSSSOString nonempty_sso_string = ls_sso_string_create(NONEMPTY_BYTES, NONEMPTY_LEN);
+	LSStringSpan nonempty_sspan = ls_sspan_create(NONEMPTY_BYTES, NONEMPTY_LEN);
+
+	LSByteBuffer empty_bbuf = ls_bbuf_create();
+
+	LSByteBuffer nonempty_bbuf = ls_bbuf_create_with_init_cap(NONEMPTY_LEN);
+	memcpy(nonempty_bbuf.bytes, NONEMPTY_BYTES, NONEMPTY_LEN);
+	nonempty_bbuf.len = NONEMPTY_LEN;
+
+	{
+		LSString from_empty = ls_string_clone(LS_EMPTY_STRING);
+		LSString from_nonempty = ls_string_clone(nonempty_string);
+		LSString from_invalid = ls_string_clone(LS_AN_INVALID_STRING);
+
+		assert(ls_string_is_valid(from_empty));
+		assert(ls_string_is_valid(from_nonempty));
+		assert(!ls_string_is_valid(from_invalid));
+
+		ls_string_destroy(&from_empty);
+		ls_string_destroy(&from_nonempty);
+	}
+	{
+		LSString from_empty = ls_string_from_short_string(LS_EMPTY_SHORT_STRING);
+		LSString from_nonempty = ls_string_from_short_string(nonempty_short_string);
+		LSString from_invalid = ls_string_from_short_string(LS_AN_INVALID_SHORT_STRING);
+
+		assert(ls_string_is_valid(from_empty));
+		assert(ls_string_is_valid(from_nonempty));
+		assert(!ls_string_is_valid(from_invalid));
+
+		ls_string_destroy(&from_empty);
+		ls_string_destroy(&from_nonempty);
+	}
+	{
+		LSString from_empty = ls_string_from_sso_string(LS_EMPTY_SSO_STRING);
+		LSString from_nonempty = ls_string_from_sso_string(nonempty_sso_string);
+		LSString from_invalid = ls_string_from_sso_string(LS_AN_INVALID_SSO_STRING);
+
+		assert(ls_string_is_valid(from_empty));
+		assert(ls_string_is_valid(from_nonempty));
+		assert(!ls_string_is_valid(from_invalid));
+
+		ls_string_destroy(&from_empty);
+		ls_string_destroy(&from_nonempty);
+	}
+	{
+		LSString from_empty = ls_string_from_sspan(LS_EMPTY_SSPAN);
+		LSString from_nonempty = ls_string_from_sspan(nonempty_sspan);
+		LSString from_invalid = ls_string_from_sspan(LS_AN_INVALID_SSPAN);
+
+		assert(ls_string_is_valid(from_empty));
+		assert(ls_string_is_valid(from_nonempty));
+		assert(!ls_string_is_valid(from_invalid));
+
+		ls_string_destroy(&from_empty);
+		ls_string_destroy(&from_nonempty);
+	}
+	{
+		LSString from_empty = ls_string_from_bbuf(empty_bbuf);
+		LSString from_nonempty = ls_string_from_bbuf(nonempty_bbuf);
+		LSString from_invalid = ls_string_from_bbuf(LS_AN_INVALID_BBUF);
+
+		assert(ls_string_is_valid(from_empty));
+		assert(ls_string_is_valid(from_nonempty));
+		assert(!ls_string_is_valid(from_invalid));
+
+		ls_string_destroy(&from_empty);
+		ls_string_destroy(&from_nonempty);
+	}
+
+	{
+		LSShortString from_empty = ls_short_string_from_string(LS_EMPTY_STRING);
+		LSShortString from_nonempty = ls_short_string_from_string(nonempty_string);
+		LSShortString from_invalid = ls_short_string_from_string(LS_AN_INVALID_STRING);
+
+		assert(ls_short_string_is_valid(from_empty));
+		assert(ls_short_string_is_valid(from_nonempty));
+		assert(!ls_short_string_is_valid(from_invalid));
+	}
+	{
+		LSShortString from_empty = ls_short_string_from_sso_string(LS_EMPTY_SSO_STRING);
+		LSShortString from_nonempty = ls_short_string_from_sso_string(nonempty_sso_string);
+		LSShortString from_invalid = ls_short_string_from_sso_string(LS_AN_INVALID_SSO_STRING);
+
+		assert(ls_short_string_is_valid(from_empty));
+		assert(ls_short_string_is_valid(from_nonempty));
+		assert(!ls_short_string_is_valid(from_invalid));
+	}
+	{
+		LSShortString from_empty = ls_short_string_from_sspan(LS_EMPTY_SSPAN);
+		LSShortString from_nonempty = ls_short_string_from_sspan(nonempty_sspan);
+		LSShortString from_invalid = ls_short_string_from_sspan(LS_AN_INVALID_SSPAN);
+
+		assert(ls_short_string_is_valid(from_empty));
+		assert(ls_short_string_is_valid(from_nonempty));
+		assert(!ls_short_string_is_valid(from_invalid));
+	}
+	{
+		LSShortString from_empty = ls_short_string_from_bbuf(empty_bbuf);
+		LSShortString from_nonempty = ls_short_string_from_bbuf(nonempty_bbuf);
+		LSShortString from_invalid = ls_short_string_from_bbuf(LS_AN_INVALID_BBUF);
+
+		assert(ls_short_string_is_valid(from_empty));
+		assert(ls_short_string_is_valid(from_nonempty));
+		assert(!ls_short_string_is_valid(from_invalid));
+	}
+
+	{
+		LSSSOString from_empty = ls_sso_string_from_string(LS_EMPTY_STRING);
+		LSSSOString from_nonempty = ls_sso_string_from_string(nonempty_string);
+		LSSSOString from_invalid = ls_sso_string_from_string(LS_AN_INVALID_STRING);
+
+		assert(ls_sso_string_is_valid(from_empty));
+		assert(ls_sso_string_is_valid(from_nonempty));
+		assert(!ls_sso_string_is_valid(from_invalid));
+
+		ls_sso_string_destroy(&from_empty);
+		ls_sso_string_destroy(&from_nonempty);
+	}
+	{
+		LSSSOString from_empty = ls_sso_string_from_short_string(LS_EMPTY_SHORT_STRING);
+		LSSSOString from_nonempty = ls_sso_string_from_short_string(nonempty_short_string);
+		LSSSOString from_invalid = ls_sso_string_from_short_string(LS_AN_INVALID_SHORT_STRING);
+
+		assert(ls_sso_string_is_valid(from_empty));
+		assert(ls_sso_string_is_valid(from_nonempty));
+		assert(!ls_sso_string_is_valid(from_invalid));
+
+		ls_sso_string_destroy(&from_empty);
+		ls_sso_string_destroy(&from_nonempty);
+	}
+	{
+		LSSSOString from_empty = ls_sso_string_clone(LS_EMPTY_SSO_STRING);
+		LSSSOString from_nonempty = ls_sso_string_clone(nonempty_sso_string);
+		LSSSOString from_invalid = ls_sso_string_clone(LS_AN_INVALID_SSO_STRING);
+
+		assert(ls_sso_string_is_valid(from_empty));
+		assert(ls_sso_string_is_valid(from_nonempty));
+		assert(!ls_sso_string_is_valid(from_invalid));
+
+		ls_sso_string_destroy(&from_empty);
+		ls_sso_string_destroy(&from_nonempty);
+	}
+	{
+		LSSSOString from_empty = ls_sso_string_from_sspan(LS_EMPTY_SSPAN);
+		LSSSOString from_nonempty = ls_sso_string_from_sspan(nonempty_sspan);
+		LSSSOString from_invalid = ls_sso_string_from_sspan(LS_AN_INVALID_SSPAN);
+
+		assert(ls_sso_string_is_valid(from_empty));
+		assert(ls_sso_string_is_valid(from_nonempty));
+		assert(!ls_sso_string_is_valid(from_invalid));
+
+		ls_sso_string_destroy(&from_empty);
+		ls_sso_string_destroy(&from_nonempty);
+	}
+	{
+		LSSSOString from_empty = ls_sso_string_from_bbuf(empty_bbuf);
+		LSSSOString from_nonempty = ls_sso_string_from_bbuf(nonempty_bbuf);
+		LSSSOString from_invalid = ls_sso_string_from_bbuf(LS_AN_INVALID_BBUF);
+		assert(ls_sso_string_is_valid(from_empty));
+		assert(ls_sso_string_is_valid(from_nonempty));
+		assert(!ls_sso_string_is_valid(from_invalid));
+
+		ls_sso_string_destroy(&from_empty);
+		ls_sso_string_destroy(&from_nonempty);
+	}
+
+	{
+		LSStringSpan from_empty = ls_sspan_from_string(LS_EMPTY_STRING);
+		LSStringSpan from_nonempty = ls_sspan_from_string(nonempty_string);
+		LSStringSpan from_invalid = ls_sspan_from_string(LS_AN_INVALID_STRING);
+
+		assert(ls_sspan_is_valid(from_empty));
+		assert(ls_sspan_is_valid(from_nonempty));
+		assert(!ls_sspan_is_valid(from_invalid));
+	}
+	{
+		LSStringSpan from_empty = ls_sspan_from_short_string(&LS_EMPTY_SHORT_STRING);
+		LSStringSpan from_nonempty = ls_sspan_from_short_string(&nonempty_short_string);
+		LSStringSpan from_invalid = ls_sspan_from_short_string(&LS_AN_INVALID_SHORT_STRING);
+
+		assert(ls_sspan_is_valid(from_empty));
+		assert(ls_sspan_is_valid(from_nonempty));
+		assert(!ls_sspan_is_valid(from_invalid));
+	}
+	{
+		LSStringSpan from_empty = ls_sspan_from_sso_string(&LS_EMPTY_SSO_STRING);
+		LSStringSpan from_nonempty = ls_sspan_from_sso_string(&nonempty_sso_string);
+		LSStringSpan from_invalid = ls_sspan_from_sso_string(&LS_AN_INVALID_SSO_STRING);
+
+		assert(ls_sspan_is_valid(from_empty));
+		assert(ls_sspan_is_valid(from_nonempty));
+		assert(!ls_sspan_is_valid(from_invalid));
+	}
+	{
+		LSStringSpan from_empty = ls_sspan_from_bbuf(empty_bbuf);
+		LSStringSpan from_nonempty = ls_sspan_from_bbuf(nonempty_bbuf);
+		LSStringSpan from_invalid = ls_sspan_from_bbuf(LS_AN_INVALID_BBUF);
+
+		assert(ls_sspan_is_valid(from_empty));
+		assert(ls_sspan_is_valid(from_nonempty));
+		assert(!ls_sspan_is_valid(from_invalid));
+	}
+
+	{
+		LSByteBuffer from_empty = ls_bbuf_from_sspan(LS_EMPTY_SSPAN);
+		LSByteBuffer from_nonempty = ls_bbuf_from_sspan(nonempty_sspan);
+		LSByteBuffer from_invalid = ls_bbuf_from_sspan(LS_AN_INVALID_SSPAN);
+
+		assert(ls_bbuf_is_valid(from_empty));
+		assert(ls_bbuf_is_valid(from_nonempty));
+		assert(!ls_bbuf_is_valid(from_invalid));
+		
+		ls_bbuf_destroy(&from_empty);
+		ls_bbuf_destroy(&from_nonempty);
+	}
+	{
+		LSByteBuffer from_empty = ls_bbuf_clone(empty_bbuf);
+		LSByteBuffer from_nonempty = ls_bbuf_clone(nonempty_bbuf);
+		LSByteBuffer from_invalid = ls_bbuf_clone(LS_AN_INVALID_BBUF);
+
+		assert(ls_bbuf_is_valid(from_empty));
+		assert(ls_bbuf_is_valid(from_nonempty));
+		assert(!ls_bbuf_is_valid(from_invalid));
+
+		ls_bbuf_destroy(&from_empty);
+		ls_bbuf_destroy(&from_nonempty);
+	}
+
+	ls_string_destroy(&nonempty_string);
+	ls_sso_string_destroy(&nonempty_sso_string);
+
+	ls_bbuf_destroy(&empty_bbuf);
+	ls_bbuf_destroy(&nonempty_bbuf);
 }
